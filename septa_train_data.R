949c542e9f56d97297d324cbbd6946d7b4959fc0
jessyarnall <62123316+jessyarnall@users.noreply.github.com>
2023-10-23 21:37:07 -0400

    add times

--- septa_train_data.R
+++ septa_train_data.R
@@ -138,7 +138,7 @@ data <- data %>%
   mutate(outbound_ind = case_when(line %like% 'Out' ~ 1,
                                   TRUE ~ 0))
 
-#lines <- data %>% select(stop) %>% distinct() #%>% group_by(stop) %>% summarise(count = n())
+#lines <- data %>% select(line) %>% distinct() #%>% group_by(stop) %>% summarise(count = n())
 
 #write_clip(lines)
 
@@ -155,4 +155,219 @@ data_update <- data %>% left_join(line_info, by= 'line') %>%
                                     late_mins <= 5 ~ 0)) %>%
   mutate_at(vars(amtrak_ind), ~replace_na(., 0))
 
-write_csv(data_update, 'septa_data.csv')
+
+sheet_names <- excel_sheets("SEPTA_ScheduleData_10-23-23.xlsx") # get sheet names
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 1,  range =cell_cols(1), col_types = 'text')
+times1 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 1, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[1]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 2,  range =cell_cols(1), col_types = 'text')
+times2 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 2, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[2]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 3,  range =cell_cols(1), col_types = 'text')
+times3 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 3, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[3]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 4,  range =cell_cols(1), col_types = 'text')
+times4 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 4, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[4]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 5,  range =cell_cols(1), col_types = 'text')
+times5 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 5, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[5]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 6,  range =cell_cols(1), col_types = 'text')
+times6 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 6, col_types = 'date') %>%
+  select(-tops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'tops') %>%
+  mutate(line = sheet_names[[6]][1])%>%
+  rename(Stops = tops)
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 7,  range =cell_cols(1), col_types = 'text')
+times7 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 7, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[7]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 8,  range =cell_cols(1), col_types = 'text')
+times8 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 8, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[8]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 9,  range =cell_cols(1), col_types = 'text')
+times9 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 9, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[9]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 10,  range =cell_cols(1), col_types = 'text')
+times10 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 10, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[10]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 11,  range =cell_cols(1), col_types = 'text')
+times11 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 11, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[11]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 12,  range =cell_cols(1), col_types = 'text')
+times12 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 12, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[12]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 13,  range =cell_cols(1), col_types = 'text')
+times13 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 13, col_types = 'date') %>%
+  select(-tops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'tops') %>%
+  mutate(line = sheet_names[[13]][1]) %>%
+  rename(Stops = tops)
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 14,  range =cell_cols(1), col_types = 'text')
+times14 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 14, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[14]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 15,  range =cell_cols(1), col_types = 'text')
+times15 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 15, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[15]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 16,  range =cell_cols(1), col_types = 'text')
+times16 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 16, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[16]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 17,  range =cell_cols(1), col_types = 'text')
+times17 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 17, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[17]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 18,  range =cell_cols(1), col_types = 'text')
+times18 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 18, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[18]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 19,  range =cell_cols(1), col_types = 'text')
+times19 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 19, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[19]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 20,  range =cell_cols(1), col_types = 'text')
+times20 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 20, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[20]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 21,  range =cell_cols(1), col_types = 'text')
+times21 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 21, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[21]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 22,  range =cell_cols(1), col_types = 'text')
+times22 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 22, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[22]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 23,  range =cell_cols(1), col_types = 'text')
+times23 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 23, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[23]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 24,  range =cell_cols(1), col_types = 'text')
+times24 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 24, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[24]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 25,  range =cell_cols(1), col_types = 'text')
+times25 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 25, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[25]][1])
+
+stops <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 26,  range =cell_cols(1), col_types = 'text')
+times26 <- read_excel("SEPTA_ScheduleData_10-23-23.xlsx", sheet = 26, col_types = 'date') %>%
+  select(-Stops) %>%
+  cbind(., stops) %>%
+  gather("train", "time", -'Stops') %>%
+  mutate(line = sheet_names[[26]][1])
+
+
+times <- rbind(times1, times2, times3, times4, times5, times6, times7, times8, times9, times10, times11, times12, times13, times14, times15, times16, times17, times18, times19, times20, times21, times22, times23, times24, times25, times26)
+
+
+# lines <- data_update_test %>% select(line) %>% distinct() #%>% group_by(stop) %>% summarise(count = n())
+# 
+# write_clip(lines)
+
+
+times_lines <- read_csv("times_lines.csv")
+
+# remove outbound_ind from times df
+times_lines <- times_lines%>% select(-outbound_ind)
+
+times_update <- times %>% left_join(times_lines, by= 'line') %>%
+  mutate(line = line_fix) %>%
+  select(-line_fix) %>%
+  # extract time only 
+  mutate(time = format(time, format = "%H:%M:%S") )
+
+## add times to df
+
+data_update_times <- data_update %>% left_join(times_update, by = c('line', 'stop' = 'Stops', `Train Number` = 'train'))
+
+
+
+#---------------------
+
+write_csv(data_update_times, 'septa_data.csv')
